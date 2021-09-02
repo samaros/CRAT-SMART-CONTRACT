cd0283e22a6d2a2ba653d991c593b9b08bf89992
nenyk <2d1n6a@gmail.com>
2021-09-02 18:12:51 +0300

    added receiver

--- contracts/CRATCrowdsale.sol
+++ contracts/CRATCrowdsale.sol
@@ -12,6 +12,8 @@ contract CRATCrowdsale is AccessControl, ReentrancyGuard {
     bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
 
     IERC20 public immutable CRAT;
+
+    address public receiver;
  
     constructor(address _CRAT) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -22,13 +24,19 @@ contract CRATCrowdsale is AccessControl, ReentrancyGuard {
     function buy(address tokenToPay, uint256 amountToPay, uint256 amountToReceive, uint256 endTime, bytes calldata signature) external payable nonReentrant {
         require(hasRole(SIGNER_ROLE, ECDSA.recover(keccak256(abi.encodePacked(tokenToPay, amountToPay, amountToReceive, endTime)), signature)));
         require(endTime >= block.timestamp);
+        require(amountToPay > 0 && amountToReceive > 0, "Cannot pay or receive zero");
         if (tokenToPay == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
             require(amountToPay == msg.value);
+            payable(receiver).transfer(msg.value);
         }
         else {
             require(msg.value == 0, "Cannot send ETH while paying with token");
-            require(IERC20(tokenToPay).transferFrom(_msgSender(), address(this), amountToPay));
+            require(IERC20(tokenToPay).transferFrom(_msgSender(), receiver, amountToPay));
         }
         require(CRAT.transfer(_msgSender(), amountToReceive));
+    }
+
+    function setReceiver(address _receiver) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        receiver = _receiver;
     }
 }
